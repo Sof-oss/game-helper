61d0f893eab4d0a8ee284c0637a9a46c32c1e89d
Sof <zic472@gmail.com>
2026-09-24 02:16:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const SETS=[
 {name:"Марафонец",bonuses:{knife:9,pistol:10,auto:11}},
 {name:"Полевой",bonuses:{grenade:16,gl:34,gauss:108}},
 {name:"Болотник",bonuses:{knife:14,pistol:14,auto:16}},
-{name:"Омон",bonuses:{grenade:30,gl:62,gauss:198},critGaussChance:.01,critGaussDamage:100},
+{name:"Омон",bonuses:{grenade:30,gl:62,gauss:198}},
 {name:"КХК-01",bonuses:{grenade:41,gl:85,gauss:270},critGaussChance:.01,critGaussDamage:100},
 {name:"Рубеж-М",bonuses:{grenade:50,gl:102,gauss:324},critChance:.02,critDamage:50},
 {name:"Научный сотрудник",bonuses:{knife:18,pistol:19,auto:22}},
